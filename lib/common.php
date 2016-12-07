e6a7230932b016f2f4a9e544009e316bda90f29d
michelebrandolin <michele.brandolin@ecaterec.commichele.brandolin@gmail.com>
2016-12-07 12:20:13 +0100

    * added to_json() function in common lib, useful to convert data fetched with phpactiverecord

--- lib/common.php
+++ lib/common.php
@@ -2,11 +2,11 @@
 
 // ~~~~~~  Global functions ~~~~~~~ //
 
-function url()
-{
 
-}
-
+/**
+ * Sends user to a new URL
+ * @param string $path the url path
+ */
 function redirect($path)
 {
     if (headers_sent()) {
@@ -20,10 +20,43 @@ function redirect($path)
 
 }
 
+/**
+ * Debug shortcut
+ * @param mixed $v whatever you want to debug
+ * @param bool $die if true it dies
+ */
 function dd($v, $die=false)
 {
     echo "<pre class='xdebug-var-dump'>";
     print_r($v);
     echo "</pre>";
     if($die) die();
+}
+
+
+/**
+ * @param $data array of result data
+ * @param string $root name of the root element
+ * @return string
+ */
+function to_json($data, $root="")
+{
+
+    $result = array();
+
+    if(is_array($data))
+    {
+        foreach($data as $row)
+        {
+            array_push($result, $row->to_array()); //using to_array instead of to_json
+        }
+
+        echo ($root!="") ? json_encode(array($root => $result)) : json_encode($result);
+
+    } else {
+
+        echo ($root!="") ? json_encode(array($root => $data)) : json_encode($data);
+
+    }
+
 }
